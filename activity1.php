cf16d514236a690d336ac5b5b20caf9c7420009a
Rex Louis ndtc <rexmicrosoft@yahoo.com.ph>
2020-10-17 11:43:38 +0800

    update

---
+++ activity1.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MIT 131</title>
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
+<body>
+
+	<div class="w3-row">
+		<div class="w3-col m12 w3-center" style="margin-top: 10rem;">
+			<img src="img/fbgroupicon.png" class="w3-circle w3-align-center" style="height: 15rem;">
+		</div>
+	</div>
+
+	<div class="w3-row">
+		<div class="w3-col m12 w3-center w3-margin-top">
+			<h1 class="w3-jumbo">Facebook Groups</h1>
+		</div>
+	</div>
+
+	<div class="w3-row" style="margin-top: 7rem;">
+		<form class="w3-container w3-padding-64">
+
+			<label class="w3-xxlarge">Facebook Login</label>
+			<input class="w3-input w3-xxlarge" type="text">
+
+			<label class="w3-xxlarge">Password</label>
+			<input class="w3-input w3-xxlarge" type="text">
+
+		</form>
+	</div>
+
+	<div class="w3-row">
+		<button class="w3-btn w3-white w3-border">Button</button>
+	</div>
+	
+</body>
+</html>
