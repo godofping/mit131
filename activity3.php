ffb2612ce6b2144f130b998150d21d069d5eaac6
D Murphy <rexmicrosoft@yahoo.com.ph>
2020-10-26 14:02:35 +0800

    Update activity3.php

---
+++ activity3.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Activity 3</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-colors-highway.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<style>
+	
+	  	body {
+	  		font-family: Arial, Helvetica, sans-serif;
+	  	}
+
+	  	.title {
+	  		margin-top: -0.7rem;
+	  		padding-bottom: 0.9rem;
+	  	}
+
+	  	.mar {
+	  		margin-left: 5px;
+	  		margin-right: 5px;
+	  	}
+
+	  	.headtext {
+	  		line-height: 20px;
+	  	}
+
+  	</style>
+
+ 
+
+</head>
+<body>
+
+	<div class="grid-container">
+		
+		<div class="w3-container">
+			<div>
+				<h5 style="float: left;">My Task</h5>
+				<button style="float: right; margin-top: 10px;"><i class="fa fa-gear input-icon w3-text-gray"></i></button>
+			</div>
+
+			<div class="w3-row w3-margin-top">
+
+			  <div class="w3-col s3">
+
+			  	<div class="w3-card w3-red w3-center w3-round mar">
+			  		<span class="w3-xxlarge">25</span>
+				  	<p class="w3-small title">OVERDUE</p>
+				</div>
+
+			  </div>
+
+			  <div class="w3-col s3">
+
+			  	<div class="w3-card w3-light-grey w3-center w3-round mar">
+			  		<span class="w3-xxlarge">5</span>
+				  	<p class="w3-small title">TODAY</p>
+				</div>
+
+			  </div>
+
+			  <div class="w3-col s3">
+
+			  	<div class="w3-card w3-light-grey w3-center w3-round mar">
+			  		<span class="w3-xxlarge">19</span>
+				  	<p class="w3-small title">THIS WEEK</p>
+				</div>
+
+			  </div>
+
+			  <div class="w3-col s3">
+
+			  	<div class="w3-card w3-light-grey w3-center w3-round mar">
+			  		<span class="w3-xxlarge">49</span>
+				  	<p class="w3-small title">TOTAL</p>
+				</div>
+
+			  </div>
+
+			
+
+			</div>
+
+		</div>
+
+		<div class="w3-panel w3-grey">
+			<br>
+		</div> 
+
+		<div class="w3-container">
+			
+			<div>
+				<h5 style="float: left;">Tasks for my Team</h5>
+				<button style="float: right; margin-top: 10px;"><i class="fa fa-gear input-icon w3-text-gray"></i></button>
+			</div>
+
+			<div class="w3-row w3-margin-top">
+
+			  <div class="w3-col s3">
+
+			  	<div class="w3-card w3-green w3-center w3-round mar">
+			  		<span class="w3-xxlarge">0</span>
+				  	<p class="w3-small title">OVERDUE</p>
+				</div>
+
+			  </div>
+
+			  <div class="w3-col s3">
+
+			  	<div class="w3-card w3-green w3-center w3-round mar">
+			  		<span class="w3-xxlarge">0</span>
+				  	<p class="w3-small title">TODAY</p>
+				</div>
+
+			  </div>
+
+			  <div class="w3-col s3">
+
+			  	<div class="w3-card w3-light-grey w3-center w3-round mar">
+			  		<span class="w3-xxlarge">13</span>
+				  	<p class="w3-small title">THIS WEEK</p>
+				</div>
+
+			  </div>
+
+			  <div class="w3-col s3">
+
+			  	<div class="w3-card w3-light-grey w3-center w3-round mar">
+			  		<span class="w3-xxlarge">25</span>
+				  	<p class="w3-small title">TOTAL</p>
+				</div>
+
+			  </div>
+
+			
+
+			</div>
+
+		</div>
+
+		<div class="w3-panel w3-grey">
+			<br>
+		</div> 
+
+		<div class="w3-container">
+			
+			<div>
+				<h5 style="float: left;">All Zenputz Tasks Nearby</h5>
+				<button style="float: right; margin-top: 10px;"><i class="fa fa-gear input-icon w3-text-gray"></i></button>
+			</div>
+
+			<div class="w3-row w3-margin-top">
+
+			  <div class="w3-col s4">
+
+			  	<div class="w3-card w3-light-grey w3-center w3-round mar">
+			  		<span class="w3-xxlarge">5</span>
+				  	<p class="w3-small title">TASKS</p>
+				</div>
+
+			  </div>
+
+			  <div class="w3-col s8">
+
+			  	<div class="w3-left w3-margin-left">
+			  		<span class="w3-large headtext">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
+				  	<p class="w3-small title">12 Gough St</p>
+				</div>
+
+			  </div>
+
+		
+			</div>
+
+		</div>
+	
+
+
+	</div>
+
+	
+</body>
+</html>
